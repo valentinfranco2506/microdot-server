7f8d69f6eedce625694e3a6f7fd2ed21a7b07074
valentinfranco2506 <101193039+valentinfranco2506@users.noreply.github.com>
2025-05-08 16:50:02 -0300

    Update app.py

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,35 @@
-# Aplicacion del servidor
+import asyncio
+from microdot import Microdot
+from microdot import send_file
+
+app = Microdot()
+
+@app.get('/')
+async def index(request):
+    return send_file('index.html')
+
+@app.get('/styles/base.css')
+async def index(request):
+    return send_file('styles/base.css')
+
+@app.get('/scripts/base.js')
+async def index(request):
+    return send_file('scripts/base.js')
+
+@app.get('/')
+async def image(request):
+    return send_file('/static/image.jpg', max_age=3600)  # in seconds
+
+app.run()
+
+async def main():
+    # start the server in a background task
+    server = asyncio.create_task(app.start_server())
+
+    # ... do other asynchronous work here ...
+
+    # cleanup before ending the application
+    await server
+
+asyncio.run(main())
+
