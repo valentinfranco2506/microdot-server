e82bef8a5eccf1ab808ce0c205280c9cfd562d6c
valentinfranco2506 <101193039+valentinfranco2506@users.noreply.github.com>
2025-05-21 16:20:49 -0300

    Update boot.py

--- actividades/ej_02/boot.py
+++ actividades/ej_02/boot.py
@@ -1,1 +1,27 @@
-# Configuracion inicial
+def connect_to(ssid, passwd) -> None:
+
+    """Conecta el microcontrolador a la red indicada.
+
+    Parameters
+    ----------
+    ssid : str
+        Nombre de la red a conectarse
+    passwd : str
+        Contraseña de la red
+    """
+    
+    import network
+    from time import sleep
+    
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print("Connecting to network...")
+        sta_if.active(True)
+        sta_if.connect(ssid, passwd)
+        while not sta_if.isconnected():
+            print(".",end="")
+            sleep(.05)
+    
+    return sta_if.ifconfig()[0]
+
+print(connect_to("Cooperadora Alumnos", ""))# Configuracion inicial
